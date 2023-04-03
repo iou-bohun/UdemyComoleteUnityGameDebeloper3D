41bf14a2f909e04871a341145a4aa839177a100a
iou_hobun <slawkd2002@gmail.com>
2023-04-03 15:53:42 +0900

    CompleteGame
    finishLecture

--- 3DMoonLander/Assets/Scripts/Oscillator.cs
+++ 3DMoonLander/Assets/Scripts/Oscillator.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     Vector3 startingPos;
-    [SerializeField] Vector3 movementVec;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
+    [SerializeField] Vector3 movementVec; // 이동 거리 조절
+    float movementFactor;
      
     private void Start()
     {
@@ -14,7 +15,18 @@ public class Oscillator : MonoBehaviour
     }
     private void Update()
     {
+        ObstacleMovement();
+    }
+
+    private void ObstacleMovement()
+    {
+        if(period <=Mathf.Epsilon) { return; }
+        float cycles = Time.time / period;// 시간에 따라서 계속 증가 
+        const float tau = Mathf.PI * 2; // 6.238
+        float rawSinWave = Mathf.Sin(cycles  *tau); //going from -1 to 1 
+
+        movementFactor = (rawSinWave + 1f) / 2; // recalculate to go from to 1 so its cleaner
         Vector3 offset = movementVec * movementFactor;
-        transform.position =startingPos +  offset;
+        transform.position = startingPos + offset;
     }
 }
